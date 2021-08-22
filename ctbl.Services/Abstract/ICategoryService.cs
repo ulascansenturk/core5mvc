d33b0f2cbc355846c7531ec720c5a9c7f7d1879d
ulascan-dev <ulascansenturk@hotmail.com>
2021-08-22 23:31:12 +0300

    Testing something

--- ctbl.Services/Abstract/ICategoryService.cs
+++ ctbl.Services/Abstract/ICategoryService.cs
@@ -16,7 +16,6 @@ namespace ctbl.Services.Abstract
         Task<IResult> Add(CategoryAddDto categoryAddDto, string createdBy);
         Task<IResult> Update(CategoryUpdateDto categoryUpdateDto,string modifiedBy);
         Task<IResult> Delete(int categoryId);
-        Task<IResult> HardDelete(int categoryId);
            
 
 
